e4d672d3223d3040f7b178fb361316c5c369b585
fbronchaly <fbronchaly@gmail.com>
2020-04-21 09:08:52 +0200

    2 files modified, 2 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule, ReactiveFormsModule, FormGroup } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AngularFireModule } from '@angular/fire';
 import { AngularFirestoreModule } from '@angular/fire/firestore';
 import { environment } from './environments/environment';
@@ -10,11 +10,18 @@ import { HelloComponent } from './hello.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { FormularioComponent } from './formulario/formulario.component';
 import { ValidatorService } from './validator.service';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule,NgbModule,AngularFirestoreModule, ReactiveFormsModule, FormGroup, AngularFireModule.initializeApp(environment.firebase)
+  imports:      [ 
+  BrowserModule,
+  FormsModule,
+  NgbModule,
+  AngularFirestoreModule,
+  ReactiveFormsModule,  
+  AngularFireModule.initializeApp(environment.firebase)
  	],
-  declarations: [ AppComponent, HelloComponent, FormularioComponent ],
+  declarations: [ AppComponent, HelloComponent, FormularioComponent, HomeComponent ],
   bootstrap:    [ AppComponent ],
   providers: [ValidatorService]
 })
